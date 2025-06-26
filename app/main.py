2f747c39f23fd69bfb942f16f779770535b1dfac
Mateusz <szanieckimateusz@gmail.com>
2025-06-26 19:51:36 +0200

    Implicating serve_cars method

--- app/main.py
+++ app/main.py
@@ -23,6 +23,15 @@ class CarWashStation:
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
+    def serve_cars(self, cars_list: list[Car]) -> float:
+        total_income = 0.0
+        for car in cars_list:
+            if car.clean_mark < self.clean_power:
+                price = self.calculate_washing_price(car)
+                total_income += price
+                self.wash_single_car(car)
+        return round(total_income, 1)
+
     def calculate_washing_price(self, car: Car) -> float:
         cost = (
             car.comfort_class
@@ -35,3 +44,4 @@ class CarWashStation:
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
+
